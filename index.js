93bc5e127f45202c14e5b1d5b7ad2de267fe9651
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-07-20 17:43:56 -0300

    Adicionado cors

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.use(bodyParser.json());
 
 const controllers = require('./controllers');
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
+
 app.use('/transactions', controllers.transaction);
 
 app.get('/', (req, res) => res.send('Hello World!'));
